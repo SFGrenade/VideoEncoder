9cce1b3c0b70d4100f1ff51b39436d21ed28ea04
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-10-18 02:04:04 +0200

    lets try without fpic, but the ldflags as one with ,

--- xmake.lua
+++ xmake.lua
@@ -89,8 +89,7 @@ target( "ffmpeg-helper" )
             add_frameworks( "AudioToolbox", { public = true } )
         end
     elseif is_plat( "linux" ) then
-        add_cxflags( "-fpic", { public = true } ) -- thanks to ffmpeg
-        add_ldflags( "-Wl", "-Bsymbolic", { public = true } ) -- thanks to ffmpeg
+        add_ldflags( "-Wl,-Bsymbolic", { public = true } ) -- thanks to ffmpeg
         add_syslinks( "dl", "pthread", { public = true } )
     elseif is_plat( "android" ) then
         add_syslinks( "dl", "android", "mediandk", { public = true } )
