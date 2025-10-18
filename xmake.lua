c602a5d54cb5c67afbb248173a2fe8438313b807
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-10-18 02:22:34 +0200

    force the ffmped linker flags

--- xmake.lua
+++ xmake.lua
@@ -89,7 +89,7 @@ target( "ffmpeg-helper" )
             add_frameworks( "AudioToolbox", { public = true } )
         end
     elseif is_plat( "linux" ) then
-        add_ldflags( "-Wl,-Bsymbolic", { public = true } ) -- thanks to ffmpeg
+        add_ldflags( "-Wl,-Bsymbolic", { force = true, public = true } ) -- thanks to ffmpeg
         add_syslinks( "dl", "pthread", { public = true } )
     elseif is_plat( "android" ) then
         add_syslinks( "dl", "android", "mediandk", { public = true } )
